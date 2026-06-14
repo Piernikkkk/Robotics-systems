d60d56b2032b8f9302145588b2a14bf73315a404
Lin <dariapermiakowa2022@gmail.com>
2026-06-14 11:27:42 +0200

    upd

--- frontend.py
+++ frontend.py
@@ -6,26 +6,23 @@ st.title("Robot Valley")
 user_command = st.text_input("Ask Valley to move to a location (e.g., 'move to (3, 4))")
 if st.button("Enter"):
     # Send a request to the backend to move the robot
-    res = requests.post("http://127.0.0.1:8000/send_command", params={"command": user_command})
+    requests.post("http://127.0.0.1:8000/send_command", json={"command": user_command})
  
 
 st.write("---")
 
-try:
-    response = requests.get("http://127.0.0.1:8000/get_target")
-    data = response.json()
-    x_position = data.get("x", 0)
-    y_position = data.get("y", 0)
-    battery = 100
-    status = "Idle"
-except Exception:
-    x_position, y_position, battery, status = 0, 0, 0, "Server Offline"
+
+response = requests.get("http://127.0.0.1:8000/get_target")
+data = response.json()
+x_position = data.get("x", 0)
+y_position = data.get("y", 0)
+battery = 100
+status = "Idle"
+
 
 st.write(f"Current Position: (x:{x_position}, y:{y_position})")
 st.write(f"Battery Level: {battery}%")
 st.write(f"Status: {status}")
 
-if st.button("Refresh"):
-    st.rerun()
 
 
--- server.py
+++ server.py
@@ -25,17 +25,20 @@ robot_target = {
 def receive_command(command: str):
     global robot_state 
     global robot_target
-    current_position = {robot_state['x']}, {robot_state['y']}
-    user_message =  (f"You are a robot Valley. Current position: {current_position}. Respond to the user {command} with an updated JSON object {robot_target} containing the new position 'x', 'y'. Reply ONLY in a JSON format. example: {{'x': 3, 'y': 4}}")
+    current_position = f"x: {robot_state['x']}, y: {robot_state['y']}"
+
+    user_message =  (f"You are a robot Valley. Current position: {current_position}."
+                     f"Respond to the user {command} ONLY with a JSON object: {{\"x\": int, \"y\": int}}. No text around."
+    )
     
     response = client.chat.completions.create(
         model = "meta-llama/llama-3-8b-instruct:free",
         messages = [
-            {"role": "system", "content": f"{user_message}"},
-            {"role": "user", "content": f"{command}"}
+            {"role": "system", "content": user_message},
+            {"role": "user", "content": command}
         ]
     )
-    ai_json_text = response.choices[0].message.content # get pure text from AI
+    ai_json_text = response.choices[0].message.content.strip().replace("```json", "").replace("```", "").strip() # get pure text from AI
     # Update the robot's target position and status based on the AI response
 
     # Turn JSON text into a Python robot_target 
@@ -51,7 +54,7 @@ def get_target():
 def update_state(state: dict):
     global robot_state
     robot_state.update(state)
-    return {"message": "State updated successfully", "new_state": robot_state}
+    return robot_state
 
 
 #### !!!!! UPDATE ROBOT STATE WHEN HE MOVES !!!!####
